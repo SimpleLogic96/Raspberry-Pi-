278fef655b7ebfc81ef64b7aa21f6b4799344358
Kevin Kim <laclips@bu.edu>
2018-11-18 09:19:09 -0500

    discover_devices.py update

--- bluetooth/discover_devices.py
+++ bluetooth/discover_devices.py
@@ -4,21 +4,18 @@ import bluetooth
 import time
 
 def check_nearby_devices():
-    nearby_devices = []
-    if (len(nearby_devices) < 1):
+
         for x in range(4):
-            nearby_devices = nearby_devices + bluetooth.discover_devices(lookup_names = True, duration = 1)
-            print('Searching for nearby devices')
-            time.sleep(1)
-            nearby_devices = nearby_devices + bluetooth.discover_devices(lookup_names = True, duration = 1)
-            print('Searching for nearby devices.')
-            time.sleep(1)
-            nearby_devices = nearby_devices + bluetooth.discover_devices(lookup_names = True, duration = 1)
-            print('Searching for nearby devices..')
-            time.sleep(1)
-            nearby_devices = nearby_devices + bluetooth.discover_devices(lookup_names = True, duration = 1)
-            print('Searching for nearby devices...')
-            time.sleep(1)
+            if (len(nearby_devices) < 1):
+                nearby_devices = bluetooth.discover_devices(lookup_names = True, duration = 10)
+                print('Searching for nearby devices')
+                time.sleep(1)
+                print('Searching for nearby devices.')
+                time.sleep(1)
+                print('Searching for nearby devices..')
+                time.sleep(1)
+                print('Searching for nearby devices...')
+                time.sleep(1)
 
     else:
         print('We found ' + str(len(nearby_devices)) + ' devices around you.')
