bcbef2f75e4432b10291c9e7fcf5081d18b21984
Kevin Kim <laclips@bu.edu>
2018-11-18 09:10:33 -0500

    findmyphone.py update

--- bluetooth/findmyphone.py
+++ bluetooth/findmyphone.py
@@ -5,9 +5,9 @@ target_address = None
 
 nearby_devices = bluetooth.discover_devices()
 
-for bdaddr in nearby_devices:
-	if target_name == bluetooth.lookup_name( bdaadr ):
-		target_address = bdaddr
+for addr in nearby_devices:
+	if target_name == bluetooth.lookup_name( aadr ):
+		target_address = addr
 		break
 if(target_address != None):
 	print('Found target bluetooth device with address: ' + str(target_address))
