8033182f4c177cf6346e6f5af7563820735502c2
Kevin Kim <laclips@bu.edu>
2018-11-18 09:46:07 -0500

    findmyphone.py update

--- bluetooth/findmyphone.py
+++ bluetooth/findmyphone.py
@@ -2,13 +2,14 @@ import bluetooth
 
 target_name = raw_input('Enter target name: ')
 #target_address = raw_input('Enter target addr: ')
-
+target_address == ''
 nearby_devices = bluetooth.discover_devices(lookup_names=True)
+
 
 for addr, name in nearby_devices:
 	if target_name == name:
 		target_address = addr
 		break
-		return target_address
 
-print('The target address of ' + target_name + ' is ' + str(target_address))
+
+print('The target address of ' + target_name + ' is ' + target_address)
